e50ca34d722422efc5ba19a158d1f230a77560e6
switer <guankaishe@gmail.com>
2015-10-03 12:38:58 +0800

    Fixed external loader resolve bug and add "jsx", "coffee" default extensions support

--- index.js
+++ index.js
@@ -73,7 +73,12 @@ function componentsBuild(options) {
             loader: "file-loader?name=[path][name]_[hash:" + HASH_LENGTH + "].[ext]"
         }]
 
-    var loaderDirectories = [path.join(__dirname, './loaders'), path.join(__dirname, './node_modules')]
+    var loaderDirectories = [
+        path.join(__dirname, './loaders'), 
+        path.join(__dirname, './node_modules'), 
+        path.join(__dirname, '../') // parent node_modules
+    ]
+
     if (options.plugins) {
         plugins = plugins.concat(options.plugins)
     }
@@ -104,7 +109,8 @@ function componentsBuild(options) {
             },
             plugins: plugins,
             resolve: {
-                modulesDirectories: options.modulesDirectories || ['c']
+                modulesDirectories: options.modulesDirectories || ['c'],
+                extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.coffee']
             }
         })
 }
